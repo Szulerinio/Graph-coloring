9efcac2d2ae0ba0c9974798bf8f476292e7e72ea
Szulerinio <szymonkozlowski48@gmail.com>
2022-03-11 16:40:19 +0100

    Add graph generation

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ console.log(
   "==============================================reloaded!=============================================="
 );
 
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 const readFile = (name) => {
   return fs.readFileSync(name, "utf8");
 };
@@ -34,4 +37,43 @@ const stringifyData = (data) => {
   return toReturn;
 };
 
-writeFile("out.txt", stringifyData(parseData(readFile("myciel4.txt"))));
+// writeFile("out.txt", stringifyData(parseData(readFile("myciel4.txt"))));
+
+const generateGraph = (size, saturation) => {
+  if (size < 1 || saturation > 100) {
+    console.log(`wprowadzono niepoprawne dane`);
+    return;
+  }
+  if (saturation < Math.ceil(2 / size)) {
+    console.log(
+      `By zapewnić spójność grafu o rozmiarze ${size} wymagana jest saturacja na poziomie ${Math.ceil(
+        2 / size
+      )}%`
+    );
+    return;
+  }
+
+  const toReturn = [...Array(size)].map(() => []); //make an array of given size, and fill it with empty arrays
+  for (let i = 0; i < toReturn.length - 1; i++) {
+    toReturn[i].push(i + 1);
+    toReturn[i + 1].push(i);
+  }
+  let verticyCount = toReturn.length - 1;
+  const maxVerticyCount = Math.ceil(((size * size - 1) / 2 / 100) * saturation);
+  while (verticyCount < maxVerticyCount) {
+    const tempX = randomInteger(0, size - 1);
+    const tempY = randomInteger(0, size - 1);
+    if (tempX != tempY && !toReturn[tempX].includes(tempY)) {
+      toReturn[tempX].push(tempY);
+      toReturn[tempY].push(tempX);
+      verticyCount++;
+    }
+  }
+
+  toReturn.forEach((element) => {
+    element.sort((a, b) => a - b);
+  });
+  return toReturn;
+};
+
+console.table(generateGraph(40, 50));
