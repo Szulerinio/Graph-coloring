4773cdf605272fd4fdfded8251a1a536a6108602
Szulerinio <szymonkozlowski48@gmail.com>
2022-03-11 16:29:26 +0100

    add stringify and parse methods

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 const fs = require("fs");
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 console.log(
   "==============================================reloaded!=============================================="
 );
 
-const data = fs.readFileSync("myciel4.txt", "utf8");
+const readFile = (name) => {
+  return fs.readFileSync(name, "utf8");
+};
+const writeFile = (name, data) => {
+  fs.writeFileSync(name, data);
+};
 
-fs.writeFileSync("out.txt", data);
+const parseData = (data) => {
+  const cleanData = data.replace(/\r\n/g, " ").trim().split(" ");
+  let toReturn = [...Array(cleanData[0] - 0)].map(() => []); //make an array of given size, and fill it with empty arrays
 
-const dane = [];
+  for (let i = 1; i < cleanData.length; i += 2) {
+    toReturn[cleanData[i] - 1].push(cleanData[i + 1] - 1);
+    toReturn[cleanData[i + 1] - 1].push(cleanData[i] - 1);
+  }
+  return toReturn;
+};
+
+const stringifyData = (data) => {
+  let toReturn = "" + data.length;
+  toReturn += "\r\n";
+  for (let i = 0; i < data.length; i++) {
+    let filteredSubArray = data[i].filter((element) => element > i);
+    for (let j = 0; j < filteredSubArray.length; j++) {
+      toReturn += `${i + 1} ${filteredSubArray[j] + 1}`;
+      toReturn += "\r\n";
+    }
+  }
+  return toReturn;
+};
+
+writeFile("out.txt", stringifyData(parseData(readFile("myciel4.txt"))));
