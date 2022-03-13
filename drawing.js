7521c5efd4f63dee4d3ae355e40c93b44de10e4d
Szulerinio <szymonkozlowski48@gmail.com>
2022-03-13 22:16:09 +0100

    Add drawing edges and graphical improvements

--- drawing.js
+++ drawing.js
@@ -1,26 +1,35 @@
 let graph;
-
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+let colors = 0;
+ctx.font = "30px Arial";
 const draw = (timeStamp) => {
-  let colors = 0;
-  for (let i = 0; i < graph.length; i++) {
-    if (graph[i][0] > colors) colors = graph[i][0];
-  }
   console.log(graph);
+
+  for (let i = 0; i < graph.length; i++) {
+    for (let j = 1; j < graph[i].length; j++) {
+      ctx.beginPath();
+      ctx.moveTo(graph[i][0].x, graph[i][0].y);
+      ctx.strokeStyle = `rgb(255,255,255)`;
+      ctx.lineTo(graph[graph[i][j]][0].x, graph[graph[i][j]][0].y);
+      ctx.closePath();
+      ctx.stroke();
+    }
+  }
+
   for (let i = 0; i < graph.length; i++) {
     ctx.beginPath();
-    ctx.arc(
-      600 + 380 * Math.sin((Math.PI * 2 * i) / graph.length),
-      400 + 380 * Math.cos((Math.PI * 2 * i) / graph.length),
-      24,
-      0,
-      2 * Math.PI
-    );
-    ctx.fillStyle = `rgb(${(255 / colors) * graph[i][0]},100,100)`;
+    ctx.arc(graph[i][0].x, graph[i][0].y, 24, 0, 2 * Math.PI);
+    ctx.fillStyle = `rgb(${
+      (255 / colors) * graph[i][0].color * (graph[i][0].color % 3)
+    },${(255 / colors) * graph[i][0].color * ((graph[i][0].color + 1) % 3)},${
+      (255 / colors) * graph[i][0].color * ((graph[i][0].color + 2) % 3)
+    })`;
     ctx.fill();
     ctx.closePath();
+    ctx.fillStyle = "black";
+    ctx.fillText("" + graph[i][0].color, graph[i][0].x - 8, graph[i][0].y + 10);
   }
   //   window.requestAnimationFrame(draw);
 };
@@ -33,5 +42,22 @@ fetch("graph.json")
     return response.json();
   })
   .then((data) => (graph = JSON.parse(data)))
-  .then(() => window.requestAnimationFrame(draw))
+  .then(() => {
+    startGraph();
+  })
   .catch((err) => console.error(`Fetch problem: ${err.message}`));
+
+const startGraph = () => {
+  for (let i = 0; i < graph.length; i++) {
+    let temp = graph[i][0];
+    graph[i][0] = {
+      color: temp,
+      x: 600 + 380 * Math.sin((Math.PI * 2 * i) / graph.length),
+      y: 450 + 380 * Math.cos((Math.PI * 2 * i) / graph.length),
+    };
+
+    if (graph[i][0].color > colors) colors = graph[i][0].color;
+  }
+
+  window.requestAnimationFrame(draw);
+};
